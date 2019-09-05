8414c937e44a2e63c02d01e5d331e27e29f381fc
tastyfreeze <boyd@townbranch.org>
2019-09-05 12:39:16 -0400

    add variables

--- test.bat
+++ test.bat
@@ -1,3 +1,10 @@
+rem make variables
+set layer=ne_10m_admin_1_states_provinces_lakes
+set states=where name in ('Kentucky','Texas','Alaska')
+set unit=sq_mi
+set factor=2.59e+6
+set output=area.csv
+
 rem Create projected Shapefile
 ogr2ogr projected.shp -sql ^
  "select name from ne_10m_admin_1_states_provinces_lakes where name in ('Texas','Alaska','Kentucky')" ^
