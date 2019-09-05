09213e530c1ccc837bdd415569293ee21fd4d27b
tastyfreeze <boyd@townbranch.org>
2019-09-05 12:40:07 -0400

    apply variable substition

--- test.bat
+++ test.bat
@@ -7,13 +7,13 @@ set output=area.csv
 
 rem Create projected Shapefile
 ogr2ogr projected.shp -sql ^
- "select name from ne_10m_admin_1_states_provinces_lakes where name in ('Texas','Alaska','Kentucky')" ^
+ "select name from %layer% %states%" ^
  -s_srs EPSG:4326 -t_srs EPSG:5070 ^
- c:\BoydsGIS\data\ne_10m_admin_1_states_provinces_lakes.shp
+ ..\data\%layer%.shp
 
 rem Use ogr2ogr internal measurement feature
 ogr2ogr output.csv -sql ^
- "select name, (OGR_GEOM_AREA/1000000) as sq_km from projected" ^
+ "select name, (ogr_geom_area/%factor%) as %unit% from projected" ^
  projected.shp
 
 rem Delete intermediate files
