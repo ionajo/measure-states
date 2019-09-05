74be9bdeb361bccd536ed8ccd6d12c594fc9ae01
tastyfreeze <boyd@townbranch.org>
2019-09-05 12:28:14 -0400

    Update info.bat

--- info.bat
+++ info.bat
@@ -1,6 +1,11 @@
+rem set variables
 set states='Kentucky', 'Texas', 'Alaska'
 set data=c:\BoydsGIS\data\ne_10m_admin_1_states_provinces_lakes.shp
+
+rem print them to the CLI
 echo looking for %states% in %data%
+
+rem Describe data
 ogrinfo -so -al ^
  -where "name in (%states%)" ^
  %data%
